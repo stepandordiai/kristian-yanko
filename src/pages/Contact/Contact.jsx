875c053127ce07038bde9273d8ff6c20b0edb833
Stepan Dordiai <stepandordiai@gmail.com>
2025-07-17 13:28:00 +0200

    Update Contact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,15 +1,38 @@
 import { Helmet } from "react-helmet-async";
+import { motion } from "framer-motion";
 import "./Contact.scss";
+
+const pageVariants = {
+	initial: { opacity: 0 },
+	animate: { opacity: 1 },
+	exit: { opacity: 0 },
+};
 
 const Contact = () => {
 	return (
 		<>
 			<Helmet>
-				<title>Kristian Yanko | Contact</title>
+				<title>Kristian Yanko | Контакти</title>
 			</Helmet>
-			<main className="contact">
-				<h1 className="contact__title">Contact</h1>
-			</main>
+			<motion.main
+				className="contact"
+				variants={pageVariants}
+				initial="initial"
+				animate="animate"
+				exit="exit"
+				transition={{ duration: 0.5 }}
+			>
+				<h1 className="contact__title">Контакти</h1>
+				<div className="contact__form-container">
+					<p style={{ fontSize: "2rem", marginBottom: 15 }}>Get in touch</p>
+					<form className="contact__form" action="">
+						<input type="text" placeholder="Ім'я" />
+						<input type="tel" placeholder="Номер телефону" />
+						<textarea name="" id="" placeholder="Повідомлення"></textarea>
+						<button type="submit">Відправити</button>
+					</form>
+				</div>
+			</motion.main>
 		</>
 	);
 };
